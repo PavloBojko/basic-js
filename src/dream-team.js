64a03bc04505c19257f3d03b1235ba8720b70e76
Boiko <Pavlo.Boiko@ukrnafta.com>
2023-04-15 17:21:02 +0300

    Dream-Team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let rez = '';
+  members.forEach(element => {
+    typeof element === 'string' && element !== '' && element.trim() !== '' ? rez += element.trim()[0].toUpperCase() : ''
+  });
+  let out = rez.split('').sort().join('')
+  if (out === '') {
+    return false
+  } else {
+    return out
+  }
 }
 
 module.exports = {
