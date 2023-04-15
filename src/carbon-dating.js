8f89f0563219ebc668286ce6894f87c2dda770ad
Boiko <Pavlo.Boiko@ukrnafta.com>
2023-04-15 15:21:50 +0300

    Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,27 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
+function dateSample(sampleActivity) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  /**
+   * t = ln(N/N0) / k,
+   * where k = 0.693 / T[1/2].
+   */
+
+  if (typeof sampleActivity === 'string') {
+    sampleActivity = Number(sampleActivity);
+
+    if (sampleActivity > 0 && sampleActivity <= MODERN_ACTIVITY) {
+      let i = 0.693 / HALF_LIFE_PERIOD;
+      let j = Math.log(MODERN_ACTIVITY / sampleActivity) / i;
+
+      return Math.ceil(j);
+    }
+  }
+
+  return false;
 }
 
 module.exports = {
