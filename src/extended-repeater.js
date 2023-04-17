52686213bf203dcf2d7d6246859cb24735f63be9
Boiko <Pavlo.Boiko@ukrnafta.com>
2023-04-17 12:31:31 +0300

    extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -14,10 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
+ * repeatTimesвстановлює кількість numberповторів str;
+ * separator є string роздільними повторами str;
+ * additionє додатковим string, який додаватиметься до кожного повторення str
+ * additionRepeatTimes встановлює кількість numberповторів addition
+ * additionSeparator є string роздільними повтореннями addition.
+ * 
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let sepor = options.additionSeparator ? options.additionSeparator : '|';
+  let rozdil = options.separator ? options.separator : '+';
+  let addMass = options.addition !== undefined ? new Array(options.additionRepeatTimes).fill(`${options.addition}`) : [];
+  let addStr = addMass.join(sepor);
+  let resultArr = new Array(options.repeatTimes).fill(`${str}${addStr}`);
+
+  return resultArr.join(rozdil);
 }
 
 module.exports = {
